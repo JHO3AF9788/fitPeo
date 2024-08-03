6d9bebdcec3a58b8eac47b0837b497b73e970898
JH03AF9788 <championankit311@gmail.com>
2024-08-04 01:37:03 +0530

    Assignment

---
+++ src/main/java/assignment/Test.java
@@ -0,0 +1,29 @@
+package assignment;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "C:/Users/champ/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.fitpeo.com/");
+		
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath("/html/body/div[1]/div/header/div/div[3]/div[6]/a/div")).click();
+		
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[contains(@type,'number')]")).sendKeys("820");
+		
+		Thread.sleep(3000);
+		
+		driver.quit();		
+		//driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[2]/div[1]/label/span[1]/input")).click();
+	}
+}
